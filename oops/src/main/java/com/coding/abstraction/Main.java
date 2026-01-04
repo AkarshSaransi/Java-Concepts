a808229b6573704c52dc8c6d6f0c6502a45bd550
AkarshSaransi <candysaransi@gmail.com>
2026-01-04 17:25:59 +0530

    anonymous class concept

--- oops/src/main/java/com/coding/abstraction/Main.java
+++ oops/src/main/java/com/coding/abstraction/Main.java
@@ -19,6 +19,20 @@ public class Main {
         // Method from Car (concrete, not abstract)
         mySedan.displayInfo();
 
+
+//Below i am using an anonymous inner class to provide an implementation for the abstract method honk() of the abstract class Car.
+
+// When i write:
+
+// Car myCar = new Car() {
+// void honk() {
+// System.out.println("Anonymous Car honking: Honk Honk!");
+// }
+// };
+
+// i am not directly creating an instance of the abstract class Car. Instead, you are creating an unnamed subclass of Car right there, and providing the required implementation for the abstract method honk(). The Java compiler generates a new class behind the scenes, and you get an object of that anonymous subclass.
+
+
         Car myCar = new Car()
          {
             void honk() {
